0922945a84196c2d236107388363404bd973d949
GARNet <149230970+GARNet543@users.noreply.github.com>
2025-01-31 12:02:39 +0000

    add database

---
+++ data/TeamMembers.js
@@ -0,0 +1,36 @@
+export const teamMember = [{
+    id:1,
+    fullName : "Jatupat Kajornchaikul",
+    designation : "",
+    describtion: "",
+    img:"img/maleProfile.jpg"
+},
+{
+    id:2,
+    fullName : "John Doe",
+    designation : "",
+    describtion: "",
+    img:"img/maleProfile.jpg"
+},
+{
+    id:3,
+    fullName : "John Doe",
+    designation : "",
+    describtion: "",
+    img:"img/maleProfile.jpg"
+},
+{
+    id:4,
+    fullName : "John Doe",
+    designation : "",
+    describtion: "",
+    img:"img/maleProfile.jpg"
+},
+{
+    id:5,
+    fullName : "John Doe",
+    designation : "",
+    describtion: "",
+    img:"img/maleProfile.jpg"
+}
+];
