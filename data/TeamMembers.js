a8a5b87cfa4355c3d8437dcb86c2099e5bfe7d5c
GARNet <149230970+GARNet543@users.noreply.github.com>
2025-02-03 10:28:21 +0000

    individual page (jtp)

--- data/TeamMembers.js
+++ data/TeamMembers.js
@@ -1,36 +1,36 @@
 export const teamMember = [{
     id:1,
     fullName : "Jatupat Kajornchaikul",
-    designation : "",
-    describtion: "",
-    img:"img/maleProfile.jpg"
+    describtion: "frontend dev & js programming",
+    img:"img/maleProfile.jpg",
+    InfoLink:"individual/Jatupat.html"
 },
 {
     id:2,
     fullName : "John Doe",
-    designation : "",
     describtion: "",
-    img:"img/maleProfile.jpg"
+    img:"img/maleProfile.jpg",
+    InfoLink:""
 },
 {
     id:3,
     fullName : "John Doe",
-    designation : "",
     describtion: "",
-    img:"img/maleProfile.jpg"
+    img:"img/maleProfile.jpg",
+    InfoLink:""
 },
 {
     id:4,
     fullName : "John Doe",
-    designation : "",
     describtion: "",
-    img:"img/maleProfile.jpg"
+    img:"img/maleProfile.jpg",
+    InfoLink:""
 },
 {
     id:5,
     fullName : "John Doe",
-    designation : "",
     describtion: "",
-    img:"img/maleProfile.jpg"
+    img:"img/maleProfile.jpg",
+    InfoLink:""
 }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -6,16 +6,15 @@ const renderTeamMembers = () => {
 
     teamMember.forEach(member => {
         const card = document.createElement('div');
-        card.className = "col-12 col-sm-6 col-md-3 card border border-top border-dark rounded-lg mx-3 mb-3";
+        card.className = "col-12 col-sm-6 col-md-3 card border border-top border-dark mx-3 mb-3";
         card.id = `member-${member.id}`;
 
         card.innerHTML = `
             <img class="card-img-top" src="${member.img}" alt="Profile picture of ${member.fullName}">
             <div class="card-body">
                 <h5 class="card-title">${member.fullName}</h5>
-                <p class="designation">${member.designation || 'No Designation'}</p>
                 <p class="card-text">${member.describtion || 'No Description'}</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="${member.InfoLink || 'individual/text.html'}" class="btn btn-primary">More info...</a>
             </div>
         `;
 
