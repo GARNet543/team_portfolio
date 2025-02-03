67904adccbadc5374f2acf6753134f9eec1bab30
GARNet <149230970+GARNet543@users.noreply.github.com>
2025-02-03 13:26:32 +0000

    Add some missing data

--- data/TeamMembers.js
+++ data/TeamMembers.js
@@ -1,14 +1,14 @@
 export const teamMember = [{
     id:66010106,
     fullName : "Jatupat Kajornchaikul",
-    describtion: "frontend & js programming",
+    describtion: "Frontend Developer",
     img:"img/garn.jpg",
     InfoLink:"individual/Jatupat/Jatupat.html"
 },
 {
     id:66010827,
     fullName : "Supawich Rattatham",
-    describtion: "support sale eng",
+    describtion: "Support Sale Engineer",
     img:"img/ake.jpg",
     InfoLink:"individual/Supawich/Supawich.html"
     
@@ -16,21 +16,21 @@ export const teamMember = [{
 {
     id:66010471,
     fullName : "Pawarisa Kaewpanma",
-    describtion: "frontend developer",
+    describtion: "Sales Engineer",
     img:"img/kan.jpg",
     InfoLink:"individual/Pawarisa/Pawarisa.html"
 },
 {
     id:66010413,
     fullName : "Natthamon Bunhai",
-    describtion: "frontend developer",
+    describtion: "Frontend Developer",
     img:"img/puifai.jpg",
     InfoLink:"individual/Natthamon/Natthamon.html"
 },
 {
     id:66011390,
     fullName : "Thiti Sriprasonk",
-    describtion: "",
+    describtion: "Hardware Engineer",
     img:"img/thiti.jpg",
     InfoLink:"individual/Thiti/Thiti.html"
 }
